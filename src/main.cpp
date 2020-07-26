12da0afffc021d9fda479bba4750d80db8167039
Victor <victor.camargo@rappi.com>
2020-07-25 23:01:27 -0500

    Base Led
    - Simple Led Class added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
-#include "Arduino.h"
+#include <Arduino.h>
+#include <rgb/simple.h>
+
+BasicRGBLed led = BasicRGBLed(11, 10, 6);
+
+void notifier(BaseLed &led) {
+    led.setColor(0x00, 0x64, 0x00);
+}
 
 void setup() {
-
+    led.init();
 }
 
 void loop() {
-    
+    led.setColor(0xFF, 0x00, 0xFF);
+    delay(1000);
+    notifier(led);
+    delay(1000);
 }
